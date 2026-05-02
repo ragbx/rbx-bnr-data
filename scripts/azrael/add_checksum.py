47b0ab9b3b755e4ea5b2f085d12fb9b887e8eee5
François Pichenot <fpichenot@ville-roubaix.fr>
2026-05-02 16:37:28 +0200

    MaJ

--- scripts/azrael/add_checksum.py
+++ scripts/azrael/add_checksum.py
@@ -3,11 +3,13 @@ from os.path import join
 import pandas as pd
 from bnr.azrael import Azrael2list
 
-df = pd.read_csv(join("data", "az", "bnr_azrael_20260502_nouuid.csv.gz"))
+date = "20260502"
+
+df = pd.read_csv(join("data", "az", f"bnr_azrael_{date}_nouuid.csv.gz"))
 
 # root_path = '\\\\ntrbx.local\mediatheque\BNR'
 root_path = "../bnr"
 print(root_path)
 az2list = Azrael2list(root_path=root_path, code_disk=None, az=df)
 az2list.get_all_checksum(new_checksum_file_name="new_checksum")
-az2list.save_list(filename=join("data", "az", "bnr_azrael_20260502_nouuid-cs.csv.gz"))
+az2list.save_list(filename=join("data", "az", "bnr_azrael_{date}_nouuid-cs.csv.gz"))
