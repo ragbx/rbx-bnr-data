f0ff8b7e02f8340211fc9cf9c12fbecae566ae99
François Pichenot <fpichenot@ville-roubaix.fr>
2026-05-02 16:14:24 +0200

    MaJ

--- scripts/azrael/01a_azrael_list.py
+++ scripts/azrael/01a_azrael_list.py
@@ -4,7 +4,7 @@ from bnr.azrael import Azrael2list
 
 """
 
-root_path = "/home/kibini/bnr"
+root_path = "/home/kibini/bnr/"
 az2list = Azrael2list(root_path=root_path, code_disk=None)
 
 az2list.list_files(checksum_md5=False)
--- scripts/azrael/azrael_compare.py
+++ scripts/azrael/azrael_compare.py
@@ -1,12 +1,13 @@
-import pandas as pd
 from os.path import join
 
-df1 = pd.read_csv(join("data", "_ref_files_20251226.csv.gz"))
-df2 = pd.read_csv(join("data", "bnr_azrael_20260411.csv.gz"))
+import pandas as pd
 
-df1 = df1[['name', 'path', 'size', 'checksum_md5', 'uuid']]
+df1 = pd.read_csv(join("results", "ref", "_ref_files_20260411.csv.gz"))
+df2 = pd.read_csv(join("data", "az", "bnr_azrael_20260502.csv.gz"))
 
-df2 = df2.merge(df1, on=['name', 'path', 'size'], how='left')
+df1 = df1[["name", "path", "size", "checksum_md5", "uuid"]]
 
-df2_inconnu = df2[df2['uuid'].isna()]
-df2_inconnu.to_csv(join("data", "bnr_azrael_20260411_nouuid.csv.gz"), index=False)
+df2 = df2.merge(df1, on=["name", "path", "size"], how="left")
+
+df2_inconnu = df2[df2["uuid"].isna()]
+df2_inconnu.to_csv(join("data", "az", "bnr_azrael_20260502_nouuid.csv.gz"), index=False)
