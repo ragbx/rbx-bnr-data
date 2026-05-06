e03dfee70e5e929e61e0bd240289fab703ca362f
François Pichenot <fpichenot@ville-roubaix.fr>
2026-05-06 10:09:24 +0200

    MaJ

--- scripts/azrael/02a_compare_ref_az-cs.py
+++ scripts/azrael/02a_compare_ref_az-cs.py
@@ -2,37 +2,120 @@ from os.path import join
 
 import pandas as pd
 
-old_ref_date = '20251226'
-new_ref_date = '20260502'
+old_ref_date = "20251226"
+new_ref_date = "20260502"
 
-new_ref_az_ko_right = pd.read_csv(join("results", "ref", "tmp", f"new_ref_az_ko_right_{new_ref_date}.csv.gz"))
-new_ref_az_ko_right.columns = ["ref_name", "ref_path", "ref_size", "ref_last_content_modification_date",
-        "ref_last_metadata_modification_date", "ref_checksum_md5", "ref_uuid"]
-new_ref_az_ko_left_cs = pd.read_csv(join("data", "az", f"bnr_azrael_{new_ref_date}_nouuid-cs.csv.gz"))
-#new_ref_az_ko_left_cs = new_ref_az_ko_left_cs.drop(columns=['filename'])
+new_ref_az_ko_right = pd.read_csv(
+    join("results", "ref", "tmp", f"new_ref_az_ko_right_{new_ref_date}.csv.gz")
+)
+new_ref_az_ko_right.columns = [
+    "ref_name",
+    "ref_path",
+    "ref_size",
+    "ref_last_content_modification_date",
+    "ref_last_metadata_modification_date",
+    "ref_checksum_md5",
+    "ref_uuid",
+]
+new_ref_az_ko_left_cs = pd.read_csv(
+    join("data", "az", f"bnr_azrael_{new_ref_date}_nouuid-cs.csv.gz")
+)
+# new_ref_az_ko_left_cs = new_ref_az_ko_left_cs.drop(columns=['filename'])
 
 # on retire les doublons cs
-new_ref_az_ko_right_csnodupl = new_ref_az_ko_right[~new_ref_az_ko_right.duplicated(subset='ref_checksum_md5', keep=False)]
-new_ref_az_ko_left_cs_csnodupl = new_ref_az_ko_left_cs[~new_ref_az_ko_left_cs.duplicated(subset='checksum_md5', keep=False)]
+new_ref_az_ko_right_csnodupl = new_ref_az_ko_right[
+    ~new_ref_az_ko_right.duplicated(subset="ref_checksum_md5", keep=False)
+]
+new_ref_az_ko_left_cs_csnodupl = new_ref_az_ko_left_cs[
+    ~new_ref_az_ko_left_cs.duplicated(subset="checksum_md5", keep=False)
+]
 
 new_ref_az = new_ref_az_ko_left_cs_csnodupl.merge(
     new_ref_az_ko_right_csnodupl,
     left_on=["name", "checksum_md5"],
     right_on=["ref_name", "ref_checksum_md5"],
-    how="left"
+    how="left",
 )
-new_ref_az_ok = new_ref_az[(~new_ref_az['ref_uuid'].isna()) & (~new_ref_az['name'].isna())]
-new_ref_az_ko = new_ref_az[(new_ref_az['ref_uuid'].isna()) & (~new_ref_az['name'].isna())]
+new_ref_az_ok = new_ref_az[
+    (~new_ref_az["ref_uuid"].isna()) & (~new_ref_az["name"].isna())
+]
+new_ref_az_ok["uuid"] = new_ref_az_ok["ref_uuid"]
+new_ref_az_ok = new_ref_az_ok[
+    [
+        "name",
+        "path",
+        "size",
+        "last_content_modification_date",
+        "last_metadata_modification_date",
+        "checksum_md5",
+        "uuid",
+    ]
+]
+new_ref_az_ko = new_ref_az[
+    (new_ref_az["ref_uuid"].isna()) & (~new_ref_az["name"].isna())
+]
+new_ref_az_ko = new_ref_az_ko[
+    [
+        "name",
+        "path",
+        "size",
+        "last_content_modification_date",
+        "last_metadata_modification_date",
+        "checksum_md5",
+        "uuid",
+    ]
+]
 
 # on essaie de gérer les doublons
-new_ref_az_ko_right_csdupl = new_ref_az_ko_right[new_ref_az_ko_right.duplicated(subset='ref_checksum_md5', keep=False)]
-new_ref_az_ko_left_cs_csdupl = new_ref_az_ko_left_cs[new_ref_az_ko_left_cs.duplicated(subset='checksum_md5', keep=False)]
+new_ref_az_ko_right_csdupl = new_ref_az_ko_right[
+    new_ref_az_ko_right.duplicated(subset="ref_checksum_md5", keep=False)
+]
+new_ref_az_ko_left_cs_csdupl = new_ref_az_ko_left_cs[
+    new_ref_az_ko_left_cs.duplicated(subset="checksum_md5", keep=False)
+]
 
-new_ref_az2 = new_ref_az_ko_left_cs_csnodupl.merge(
-    new_ref_az_ko_right_csnodupl,
+new_ref_az2 = new_ref_az_ko_left_cs_csdupl.merge(
+    new_ref_az_ko_right_csdupl,
     left_on=["name", "checksum_md5"],
     right_on=["ref_name", "ref_checksum_md5"],
-    how="left"
+    how="left",
 )
-new_ref_az2_ok = new_ref_az2[(~new_ref_az2['ref_uuid'].isna()) & (~new_ref_az2['name'].isna())]
-new_ref_az2_ko = new_ref_az2[(new_ref_az2['ref_uuid'].isna()) & (~new_ref_az2['name'].isna())]
+new_ref_az2_ok = new_ref_az2[
+    (~new_ref_az2["ref_uuid"].isna()) & (~new_ref_az2["name"].isna())
+]
+new_ref_az2_ok["uuid"] = new_ref_az2_ok["ref_uuid"]
+new_ref_az2_ok = new_ref_az2_ok[
+    [
+        "name",
+        "path",
+        "size",
+        "last_content_modification_date",
+        "last_metadata_modification_date",
+        "checksum_md5",
+        "uuid",
+    ]
+]
+new_ref_az2_ko = new_ref_az2[
+    (new_ref_az2["ref_uuid"].isna()) & (~new_ref_az2["name"].isna())
+]
+new_ref_az2_ko = new_ref_az2_ko[
+    [
+        "name",
+        "path",
+        "size",
+        "last_content_modification_date",
+        "last_metadata_modification_date",
+        "checksum_md5",
+        "uuid",
+    ]
+]
+
+r = pd.read_csv(join("results", "ref", "tmp", "new_ref_az_ok_20260502.csv.gz"))
+r = pd.concat([r, new_ref_az_ok, new_ref_az2_ok])
+r.to_csv(
+    join("results", "ref", "tmp", "new_ref_az_ok_20260502_it2.csv.gz"), index=False
+)
+
+pd.concat([new_ref_az_ko, new_ref_az2_ko]).to_csv(
+    join("results", "ref", "tmp", "new_ref_az_ko_left_20260502_it2.csv.gz"), index=False
+)
